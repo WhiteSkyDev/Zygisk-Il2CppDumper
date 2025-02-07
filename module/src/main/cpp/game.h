c6ad384cf0f40d111cd2c420285f15435de4422a
White_Sky <107893681+WhiteSkyDev@users.noreply.github.com>
2025-02-07 20:59:24 +0300

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.th3project.standleo"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
